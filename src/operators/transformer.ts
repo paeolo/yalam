1f0250e3fc8bf34fa0d23eda516d47b42745d536
paul.lecouteur <paul.lecouteur@hoistgroup.com>
2020-12-10 21:54:57 +0100

    chore: use rxjs 7 (beta).

--- src/operators/transformer.ts
+++ src/operators/transformer.ts
@@ -1,12 +1,15 @@
+import { OperatorFunction } from 'rxjs';
+import { map } from 'rxjs/operators';
 import {
   Asset,
-  OperatorFunction
 } from '../types';
 
 interface Transformer {
-
+  transform(asset: Asset): Asset;
 }
 
 export const transform = (transformer: Transformer): OperatorFunction<Asset, Asset> => (source) => {
-  return source;
+  return source.pipe(
+    map(value => value)
+  );
 }
