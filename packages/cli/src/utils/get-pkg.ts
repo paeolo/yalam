58e5abb5eb3abce2f97731c7717fed6d2acefc5e
paul.lecouteur <paul.lecouteur@hoistgroup.com>
2021-01-30 20:02:43 +0100

    feat: add getTask

--- packages/cli/src/utils/get-pkg.ts
+++ packages/cli/src/utils/get-pkg.ts
@@ -1,14 +1,35 @@
-import fsAsync from 'fs/promises';
 import path from 'path';
+import {
+  DirectoryPath
+} from '@yalam/core';
 
-export const getPKG = async (directory: string) => {
-  const pkgPATH = path.join(directory, 'package.json');
+import {
+  RunnerMode
+} from '../runner';
+
+export const getPKG = (directory: string) => {
+  const pkgPATH = path.resolve(directory);
 
   try {
-    return JSON.parse(
-      (await fsAsync.readFile(pkgPATH)).toString()
+    return require(
+      path.join(pkgPATH, 'package.json')
     );
   } catch {
     throw new Error(`"package.json" not found: ${directory}`)
   }
+}
+
+export const getTask = (directory: DirectoryPath, mode: RunnerMode): string => {
+  const pkg = getPKG(directory);
+
+  if (!pkg.config
+    || !pkg.config.yalam
+    || !pkg.config.yalam[mode]
+    || typeof pkg.config.yalam[mode] !== 'string') {
+    throw new Error(
+      `Your "package.json" at ${directory} should provide a task name at path config.yalam.${mode}`
+    );
+  }
+
+  return pkg.config.yalam[mode];
 }
